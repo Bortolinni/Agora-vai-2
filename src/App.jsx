f3794e902dd034c9e85c5e948938be6f548214a0
MatheusPBC <matheuspbrackmannc@gmail.com>
2025-06-11 19:22:25 -0300

    Codigo base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-center text-blue-600 p-8">
+        Gerenciador de Documentos
+      </h1>
+    </div>
+  )
+}
+
+export default App;
